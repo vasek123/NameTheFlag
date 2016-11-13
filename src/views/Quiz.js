1746b476b2e6372273b0f2fe20e046205d995ceb
vasek123 <hlavac.vaclav@gmail.com>
2016-11-13 13:51:10 +0100

    change flag on arrow up

--- src/views/Quiz.js
+++ src/views/Quiz.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Flag from '../components/Flag.js';
 import Button from '../components/Button.js';
 
+//Rewrite so that currentFlag, availableFlags are contained in Quiz view state and not in App
+
 class Quiz extends Component {
   constructor(props) {
     super(props);
@@ -13,14 +15,26 @@ class Quiz extends Component {
 
     this.inputOnChange = this.inputOnChange.bind(this);
     this.clearInput = this.clearInput.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
   }
 
-  inputOnChange(e) {
-    this.setState(Object.assign({}, { input: e.target.value }));
+  inputOnChange(event) {
+    this.setState(Object.assign({}, { input: event.target.value }));
   }
 
-  clearInput(b) {
-    if (!b) return;
+  onKeyDown(event) {
+    //check which key has been pressed
+    let key = event.key;
+    if (key === "Enter") this.clearInput(this.props.checkAnswer(this.state.input));
+
+    if (key === "ArrowUp") {
+      this.props.changeCurrentFlag();
+      this.clearInput(true);
+    }
+  }
+
+  clearInput(clear) {
+    if (!clear) return;
 
     this.setState(Object.assign({}, this.state, { input: '' }));
     this.textInput.focus();
@@ -28,7 +42,7 @@ class Quiz extends Component {
 
   render() {
     return (
-      <div className="quiz view">
+      <div className="quiz view" onKeyDown={this.onKeyDown}>
         <p>Quiz view</p>
         <Flag flag={this.props.flag} />
         <input type="text" ref={(input) => this.textInput = input} value={this.state.input} onChange={this.inputOnChange} />
